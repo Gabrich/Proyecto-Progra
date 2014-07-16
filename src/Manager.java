8ee094c59eedc3a1847e5028be4d3ff844f982eb
Administrador <Administrador@WIN-7Q0AHV9SGA2>
2014-07-16 17:33:51 -0400

    Cosas

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,6 @@
 
 public class Manager {
-    
-    
+        
     Login login;
     Stock stock;
     Punto_de_Ventas ptoVenta;
@@ -9,9 +8,9 @@ public class Manager {
 
     public Manager() {
        login = new Login(this);
-       stock=new Stock(this);
-       ptoVenta=new Punto_de_Ventas(this);
-      adm=new Administrador(this);
+       stock = new Stock(this);
+       ptoVenta = new Punto_de_Ventas(this);
+       adm = new Administrador(this);
        login.setVisible(true);
     }
     
